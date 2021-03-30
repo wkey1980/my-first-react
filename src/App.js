c4c26e121899d772b1d7078a4868221374a4842a
will k <28814921+wkey1980@users.noreply.github.com>
2021-03-30 14:53:25 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
-import './App.css';
+import Navbar from './custom-components/Navbar';
+import Home from './custom-components/Home';
 
 function App() {
 	return (
 		<div className="App">
+			<Navbar />
 			<div className="content">
-				
+				<Home />
 			</div>
 		</div>
 	);
